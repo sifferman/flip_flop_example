09264cca9425f81edffb219121b2f7ff9e815cd0
Ethan Sifferman <ethan@sifferman.dev>
2024-03-02 20:44:02 -0800

    final

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Flip-Flop Example
+
+Download required tools from [OSS CAD Suite](https://github.com/YosysHQ/oss-cad-suite-build/releases/latest).
+
+```bash
+make
+```
--- counter_tb.sv
+++ counter_tb.sv
@@ -1,7 +1,7 @@
 
 module counter_tb;
 
-    logic       clk;
+    logic       clk, rst_n;
     logic [1:0] count_o;
 
     counter counter (.*);
@@ -17,7 +17,11 @@ module counter_tb;
     initial begin
         $dumpfile("dump.vcd");
         $dumpvars;
-        counter.count_q = 0;
+
+        rst_n = 0;
+        @(negedge clk);
+        rst_n = 1;
+
         repeat (20) @(negedge clk);
         $finish;
     end
